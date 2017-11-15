2ddfc917d3fe6341efd22424b678fab814c61f0c
Jorge <jorge-vale1@hotmail.com>
2017-11-15 10:27:53 +0000

    oportnidade de venda, missing List all

---
+++ example/FirstREST/Lib_Primavera/Model/OportunidadeVenda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class OportunidadeVenda
+    {
+        public string OportunidadeID
+        {
+            get;
+            set;
+        }
+        public string DescricaoOp
+        {
+            get;
+            set;
+        }
+        public string Resumo
+        {
+            get;
+            set;
+        }
+        public string Entidade
+        {
+            get;
+            set;
+        }
+        public string TipoEntidade
+        {
+            get;
+            set;
+        }
+        public DateTime Data
+        {
+            get;
+            set;
+        }
+        public string Vendedor
+        {
+            get;
+            set;
+        }
+        public string Origem
+        {
+            get;
+            set;
+        }
+        public string CicloDeVenda
+        {
+            get;
+            set;
+        }
+        public string Zona
+        {
+            get;
+            set;
+        }
+    }
+}
