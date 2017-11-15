5418af39a0758f76e0882e52f5b8ee35b99fd880
Jorge <jorge-vale1@hotmail.com>
2017-11-15 11:41:36 +0000

    zona

---
+++ example/FirstREST/Controllers/ZonaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+    public class ZonaController : ApiController
+    {
+        public Zona Get(string id)
+        {
+            Lib_Primavera.Model.Zona zona = Lib_Primavera.PriIntegration.GetZona(id);
+            if (zona == null)
+            {
+                throw new HttpResponseException(
+                  Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            else
+            {
+                return zona;
+            }
+        }
+
+    }
+}
